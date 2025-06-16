909c1e6eff2896d643800b1b7f5e43b916e5fbe9
Hungitdev <hungitdev@gmail.com>
2025-06-16 17:04:09 -0600

    project done

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 const Gameboard = (function () {
+  //logic variables
   let board = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ];
-  const getBoard = function () {
-    return board;
-  };
-  //Control logic of tic tac toe
-  function gameController() {
-    let players = [
-      {
-        name: "player1",
-        symbol: "X",
-      },
-      {
-        name: "player2",
-        symbol: "O",
-      },
-    ];
-    let winner;
-    let foundWinner = false;
-    let activePlayer = players[0];
-    //functions logic
 
-    const showActivePlayer = function () {
-      return activePlayer;
-    };
+  let players = [
+    {
+      name: "player1",
+      symbol: "X",
+    },
+    {
+      name: "player2",
+      symbol: "O",
+    },
+  ];
+  let winner;
+  let foundWinner = false;
+  let activePlayer = players[0];
+
+  //Control logics of tic tac toe
+  function gameController() {
     const switchActivePlayer = function () {
       activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
@@ -57,8 +52,7 @@ const Gameboard = (function () {
           (board[1][1] === activePlayer.symbol) &
           (board[2][0] === activePlayer.symbol)
       ) {
-        winner = activePlayer;
-        console.log("Winner is: " + activePlayer.name);
+        winner = Object.assign({}, activePlayer);
         foundWinner = true;
       }
     };
@@ -68,6 +62,12 @@ const Gameboard = (function () {
         [0, 0, 0],
         [0, 0, 0],
       ];
+      for (let i = 0; i < 9; i++) {
+        divList[i].textContent = "";
+      }
+      winnerDisplay.textContent = "";
+      uiBoard.style.pointerEvents = "auto";
+      uiBoard.style.opacity = 1;
       activePlayer = players[0];
       foundWinner = false;
     };
@@ -83,50 +83,129 @@ const Gameboard = (function () {
         (board[2][1] !== 0) &
         (board[2][2] !== 0)
       ) {
-        console.log(board);
-        restart();
-        console.log("TIE");
-        console.log("Player for new game is: " + activePlayer.name);
+        winnerDisplay.textContent = "TIE";
+        uiBoard.style.pointerEvents = "none";
+        uiBoard.style.opacity = 0.5;
       }
     };
-    //function play
+    //function select cells for both players
     const select = function (a, b) {
-      //if cell hasn't been selected
-      if (board[a][b] === 0) {
-        board[a][b] = activePlayer.symbol;
-        console.log("the player just selected is: " + activePlayer.name);
-        checkForWinner();
-        switchActivePlayer();
+      board[a][b] = activePlayer.symbol;
+      checkForWinner();
+      switchActivePlayer();
 
-        //restart game if found winner
-        if (foundWinner === true) {
-          restart();
-          console.log("game done!");
-          console.log("Player for new Round: " + activePlayer.name);
-        }
-        //restart game and declare Tie if all cells have been selected and there is no winner
-        if (foundWinner === false) {
-          declareTie();
-        }
+      //restart game if found winner
+      if (foundWinner === true) {
+        winnerDisplay.textContent = `Winner for this round is ${winner.name}`;
+        uiBoard.style.pointerEvents = "none";
+        uiBoard.style.opacity = 0.5;
+      }
+      //restart game and declare Tie if all cells have been selected and there is no winner
+      if (foundWinner === false) {
+        declareTie();
+      }
+    };
+
+    return { select, restart };
+  }
+  const controller = gameController(); //run logics
+
+  //Display and Render on web page
+  //DOM variables
+
+  const divList = document.querySelectorAll(".cell");
+  const winnerDisplay = document.querySelector(".winner-display");
+  const uiBoard = document.querySelector(".container");
+  const dialog = document.querySelector("dialog");
+  const startButton = document.querySelector("#start-button");
+  const changeNameButton = document.querySelector("#change-name-button");
+  const submitButton = document.querySelector("#submit-button");
+  const closeButton = document.querySelector("#close-button");
+  const restartButton = document.querySelector("#restart-button");
+  const player1Name = document.querySelector("#player1-name");
+  const player2Name = document.querySelector("#player2-name");
+
+  function displayController() {
+    //display UIboard for user when clicking "start" button
+    startButton.addEventListener("click", () => {
+      uiBoard.style.display = "grid";
+    });
+    //display Form for changing users' names
+    changeNameButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+    //Change name for players when clicking in form
+    submitButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (player1Name.value === "" || player2Name.value === "") {
+        alert("please fill out the entire form!");
         return;
       }
-      //if select selected cell
-      if (board[a][b] !== 0) {
-        console.log("This cell has been selected!");
-        console.log("the current player is: " + activePlayer.name);
-      }
-    };
+      players[0].name = player1Name.value;
+      players[1].name = player2Name.value;
 
-    return { showActivePlayer, select };
+      dialog.close();
+    });
+    //close the form when clicking
+    closeButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      dialog.close();
+    });
+    //restart Game when clicking
+    restartButton.addEventListener("click", () => {
+      controller.restart();
+    });
+    //Let user interact with board, render logic
+    const display = function () {
+      divList.forEach((div) => {
+        div.addEventListener("click", function (event) {
+          // Checks for selected cells first
+          if (event.target.textContent === "") {
+            if (event.target === divList[0]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(0, 0);
+            }
+            if (event.target === divList[1]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(0, 1);
+            }
+            if (event.target === divList[2]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(0, 2);
+            }
+            if (event.target === divList[3]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(1, 0);
+            }
+            if (event.target === divList[4]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(1, 1);
+            }
+            if (event.target === divList[5]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(1, 2);
+            }
+            if (event.target === divList[6]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(2, 0);
+            }
+            if (event.target === divList[7]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(2, 1);
+            }
+            if (event.target === divList[8]) {
+              event.target.textContent = activePlayer.symbol;
+              controller.select(2, 2);
+            }
+          }
+        });
+      });
+    };
+    return { display };
   }
-  return { getBoard, gameController };
+
+  return { gameController, displayController };
 })();
 
-const playRound = Gameboard.gameController();
-playRound.select(0, 0);
-playRound.select(0, 2);
-playRound.select(1, 1);
-playRound.select(2, 2);
-playRound.select(2, 1);
-playRound.select(1, 2);
-console.log(Gameboard.getBoard());
+const play = Gameboard.displayController().display;
+play();
